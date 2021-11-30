fc60703ffa10060520f81ca5d1c1c56deb37a410
Jan Hrubý <johnh@vitrium.com>
2021-11-30 10:57:25 +0100

    cog wheel change to dots

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -78,7 +78,7 @@ export default {
           },
           hidden: ['small-mobile']
         },
-        { type: 'toggleElementButton', dataElement: 'menuButton', element: 'menuOverlay', img: 'icon-header-settings-line', title: 'component.menuOverlay', hidden: ['small-mobile'] },
+        { type: 'toggleElementButton', dataElement: 'menuButton', element: 'menuOverlay', img: 'icon-tools-more', title: 'component.menuOverlay', hidden: ['small-mobile'] },
         {
           type: 'actionButton',
           dataElement: 'moreButton',
@@ -94,7 +94,7 @@ export default {
       'small-mobile-more-buttons': [
         { type: 'toggleElementButton', dataElement: 'searchButton', element: 'searchPanel', img: 'icon-header-search', title: 'component.searchPanel' },
         { type: 'toggleElementButton', dataElement: 'toggleNotesButton', element: 'notesPanel', img: 'icon-header-chat-line', title: 'component.notesPanel' },
-        { type: 'toggleElementButton', dataElement: 'menuButton', element: 'menuOverlay', img: 'icon-header-settings-line', title: 'component.menuOverlay' },
+        { type: 'toggleElementButton', dataElement: 'menuButton', element: 'menuOverlay', img: 'icon-tools-more', title: 'component.menuOverlay' },
         { type: 'spacer' },
         {
           type: 'actionButton',
