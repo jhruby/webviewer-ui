f908fb169e2dbfa6e9dcec1e589cf9246d9cafe1
radekh <radekh@vitrium.com>
2023-07-10 13:41:58 +0200

    VA-9197 fixed setting style to rectangles also with multiselect

--- src/components/NotesPanel/MultiSelectControls.js
+++ src/components/NotesPanel/MultiSelectControls.js
@@ -190,7 +190,7 @@ const MultiSelectControls = ({
         <Button
           dataElement={DataElements.NOTE_MULTI_STYLE_BUTTON}
           img="icon-menu-style-line"
-          disabled={modifiableMultiSelectAnnotations.length === 0}
+          disabled={multiSelectedAnnotations.length === 0}
           onClick={() => {
             setShowMultiStyle(!showMultiStyle);
           }}
@@ -198,7 +198,7 @@ const MultiSelectControls = ({
         />
         {showMultiStyle &&
           <MultiStylePopup
-            annotations={modifiableMultiSelectAnnotations}
+            annotations={multiSelectedAnnotations}
             triggerElementName="multiStyleButton"
             onClose={() => {
               setShowMultiStyle(false);
