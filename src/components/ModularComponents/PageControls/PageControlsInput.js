63c43d74182813046e3844bdd010da8eaa31ac65
radekh <radekh@vitrium.com>
2025-11-04 12:44:52 +0100

    VA-12210 page identifier styling changed

--- src/components/ModularComponents/PageControls/PageControlsInput.js
+++ src/components/ModularComponents/PageControls/PageControlsInput.js
@@ -93,11 +93,11 @@ const PageControlsInput = forwardRef((props, ref) => {
   };
 
   const style = {};
-  if (isFocused) {
+  //if (isFocused) {
     style.width = inputWidth;
-  } else {
-    style.width = inputWidth - 10;
-  }
+  //} else {
+    //style.width = inputWidth - 10;
+  //}
 
   const formInput =
     <form
